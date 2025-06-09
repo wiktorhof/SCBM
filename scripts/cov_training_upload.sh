86310403a368c40f50fff72de4f87aac6bbc8e05
Wiktor Hoffmann <wiktorh@student.ethz.ch>
2025-06-09 14:08:30 +0200

    Make a script to submit a batch job for global covariance training.

--- scripts/cov_training_upload.sh
+++ scripts/cov_training_upload.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-
+eval "$(conda shell.bash hook)"
+conda activate scbm
 today=$(date +%y-%m-%d)
 
-output_file=/cluster/home/wiktorh/Desktop/scbm/slurm_outputs/$today/job-%J.txt
+export WANDB_API_KEY=local-be0546acbdda04d2949d57a384bcb9552c9aede7
+output_dir=/cluster/home/wiktorh/Desktop/scbm/slurm_outputs/$today
+output_file=${output_dir}/job-%J.txt
 
 data='CUB'
 mem='20G'
 encoder_arch='resnet18'
-model='CBM'
-i=42
+model='PSCBM'
+i=9431
 concept_learning='hard'
-tag=${model}_${concept_learning}
+cov='global'
 
 save_model='True'
 save_model_dir=/cluster/work/vogtlab/Group/wiktorh/PSCBM/models/
+cd /cluster/home/wiktorh/Desktop/scbm/scripts/
+echo Submitting job
 
-sbatch --output=${output_file} --mem=$mem train.sh +model=$model +data=$data \
-experiment_name=${data}_${model}_global_${i} \
-+model.CBM_dir=/cluster/work/vogtlab/Group/wiktorh/PSCBM/models/cbm/hard/CUB/20250402-115252_3c2ab/model.pth \
-seed=$i logging.project=PSCBM logging.mode=offline model.tag=$tag \
+data_ratio=1
+covariance_scaling=1.0001
+
+tag=${model}_${concept_learning}_${cov}
+sbatch --output=${output_file} --job-name=${tag} --mem=$mem train.sh +model=$model \
++data=$data experiment_name="${data}_${tag}_${i}" seed=$i model.tag=$tag \
 model.concept_learning=$concept_learning model.encoder_arch=$encoder_arch \
-model.cov_type=global save_model=${save_model} experiment_dir=${save_model_dir} \
-model.reg_precision=l1 model.covariance_scaling=1.0001 model.load_weights=True \
-model.inter_strategy='hard' model.inter_policy='random,prob_unc'
+save_model=${save_model} experiment_dir=${save_model_dir} model.load_weights=True \
+model.cov_type=${cov} model.data_ratio=${data_ratio} model.covariance_scaling=${covariance_scaling} \
+model.inter_policy=\'prob_unc,random\' model.inter_strategy=\'hard,simple_perc,emp_perc,conf_interval_optimal\' \
+model.training_mode='joint'
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-<<<<<<< HEAD
-#Lines that start with SBATCH are merely comments for bash. I cannot use variables there
-#SBATCH --job-name=CBM
-#SBATCH --output="/cluster/home/wiktorh/Desktop/scbm/slurm_outputs/job-%J"
-||||||| b483e79
-
-now=$(date +%y:%m:%d:%H:%M)
-output_filename=~/Desktop/scbm/job_outputs/$now.txt
-
-#Lines that start with SBATCH are merely comments for bash. I cannot use variables there
 #SBATCH --job-name=CBM
 #SBATCH --output="/cluster/home/wiktorh/Desktop/scbm/slurm_outputs/job-%J"
 #SBATCH --cpus-per-task=2
 #SBATCH --time=7-00:00:00
 #SBATCH -p gpu
 #SBATCH --gres=gpu:1
+#Lines that start with SBATCH are merely comments for bash. I cannot use variables there
+#Lines that start with SBATCH are merely comments for bash. I cannot use variables there
+
+now=$(date +%y:%m:%d:%H:%M)
+output_filename=~/Desktop/scbm/job_outputs/$now.txt
 
 cd /cluster/home/wiktorh/Desktop/scbm
 echo $(which python)
